621afaec0b5d18c350928cc1dc7f52665ee0ba4d
ymozer <yusufmetinozer@gmail.com>
2023-04-04 02:38:39 +0300

    Classes implemented

--- publisher.py
+++ publisher.py
@@ -5,71 +5,76 @@ import time
 import pandas as pd
 import numpy as np
 
-def csv_read():
-	df = pd.read_csv("T1.csv")
-	# Renaming columns for convenience
-	df.rename(columns={'Date/Time': 'DateTime'}, inplace=True)
-	df.rename(columns={'LV ActivePower (kW)': 'ActivePower'}, inplace=True)
-	df.rename(columns={'Wind Speed (m/s)': 'Windspeed'}, inplace=True)
-	df.rename(columns={'Wind Direction (°)': 'Winddir'}, inplace=True)
-	df.rename(columns={'Theoretical_Power_Curve (KWh)': 'TheoreticalPC'}, inplace=True)
-	
-	# Create seperate dataframes for sendinfg to different subs
-	# Each consist the data/time column
-	df_ActivePower 		= df.drop(columns=["Windspeed","Winddir","TheoreticalPC"])
-	df_WindSpeed 			= df.drop(columns=["ActivePower","Winddir","TheoreticalPC"])
-	df_WindDir 				= df.drop(columns=["Windspeed","ActivePower","TheoreticalPC"])
-	df_TheoreticalPC	= df.drop(columns=["Windspeed","Winddir","ActivePower"])
+class Publisher:
+	@classmethod
+	async def create(cls,data, name, delay):
+		self = Publisher()
+		self.data=data
+		self.name=name
+		self.delay=delay
 
-	# Return seperate dataframes as tuples
-	data = [
-		df_ActivePower,
-		df_WindSpeed,
-		df_WindDir,
-		df_TheoreticalPC
-	]
-	return data
+		print(f"started at {time.strftime('%X')}")
+		asyncio.run(await self.publisherAgent(self.data, self.name, self.delay))
+		print(f"finished at {time.strftime('%X')}")
+		asyncio.run(await self.publisherAgent(("STOP"), self.name, self.delay))
+		return self
+	async def publisherAgent(self,data:pd.DataFrame, node:str, delay:int):
+		# Connection to redis machine
+		r = redis.Redis(host='localhost', port=6379, db=0)
+		print(f"{node} ping successful: {await r.ping()}")
+		# Sending 'STOP' signal to subs if file ends
+		if type(data) == str and  data == "STOP":
+			count=0
+			while count<100:
+				print(f"[{time.strftime('%X')}][{count}]: Sending stop signal for {node}. ")
+				await r.publish(node, data) # send stop signal 
+				count+=1
+			await r.close()
 
-async def publisherAgent(data:pd.DataFrame, node:str, delay:int):
-	# Connection to redis machine
-	r = redis.Redis(host='localhost', port=6379, db=0)
-	print(f"{node} ping successful: {await r.ping()}")
-	# Sending 'STOP' signal to subs if file ends
-	if type(data) == str and  data == "STOP":
-		count=0
-		while count<100:
-			print(f"[{time.strftime('%X')}][{count}]: Sending stop signal for {node}. ")
-			await r.publish(node, data) # send stop signal 
-			count+=1
+		# Data publishing
+		for index, row in data.iterrows():
+			to_be_published_str = f"{row[0]}\n{row[1]}"
+			# publish dataframe to specified node
+			await r.publish(node, to_be_published_str)
+			# Sleep async for specified seconds
+			await asyncio.sleep(delay)
 		await r.close()
+'''END OF CLASS'''
 
-	# Data publishing
-	for index, row in data.iterrows():
-		to_be_published_str = f"{row[0]}\n{row[1]}"
-		# publish dataframe to specified node
-		await r.publish(node, to_be_published_str)
-		# Sleep async for specified seconds
-		await asyncio.sleep(delay)
-	await r.close()
+def csv_read(data):
+		df = pd.read_csv(data)
+		# Renaming columns for convenience
+		df.rename(columns={'Date/Time': 'DateTime'}, inplace=True)
+		df.rename(columns={'LV ActivePower (kW)': 'ActivePower'}, inplace=True)
+		df.rename(columns={'Wind Speed (m/s)': 'Windspeed'}, inplace=True)
+		df.rename(columns={'Wind Direction (°)': 'Winddir'}, inplace=True)
+		df.rename(columns={'Theoretical_Power_Curve (KWh)': 'TheoreticalPC'}, inplace=True)
+		
+		# Create seperate dataframes for sendinfg to different subs
+		# Each consist the data/time column
+		df_ActivePower 		= df.drop(columns=["Windspeed","Winddir","TheoreticalPC"])
+		df_WindSpeed 			= df.drop(columns=["ActivePower","Winddir","TheoreticalPC"])
+		df_WindDir 				= df.drop(columns=["Windspeed","ActivePower","TheoreticalPC"])
+		df_TheoreticalPC	= df.drop(columns=["Windspeed","Winddir","ActivePower"])
+
+		# Return seperate dataframes as tuples
+		df_list = [
+			df_ActivePower,
+			df_WindSpeed,
+			df_WindDir,
+			df_TheoreticalPC
+		]
+		return df_list
 
 
-async def main(data,delay):
-	print(f"started at {time.strftime('%X')}")
-	# TODO : Don't publish just one column, publish all columns for row
-	# Need to compare this data if they have same date
-	await asyncio.gather(	publisherAgent(data[0], "node-1", delay),
-												publisherAgent(data[1], "node-2", delay),
-												publisherAgent(data[2], "node-3", delay)
-	)
-	print(f"finished at {time.strftime('%X')}")
-	await publisherAgent(("STOP"), "node-1", delay)
-	await publisherAgent(("STOP"), "node-2", delay)
-	await publisherAgent(("STOP"), "node-3", delay)
+async def main():
+	# Read csv file
+	filename = "T1.csv"
+	delay = 1
+	data=csv_read(filename)
+	# Create publishers
+	await asyncio.gather(Publisher.create(data[0],'node-1',delay),Publisher.create(data[1],'node-2',delay),Publisher.create(data[2],'node-3',delay))
 
 
 if __name__ == "__main__":
-	# dataset parse
-	data = csv_read()
-	# delay in seconds
-	delay = 1
-	asyncio.run(main(data,delay))
+	asyncio.run(main())
--- subs.py
+++ subs.py
@@ -3,85 +3,90 @@ import aiofiles
 import redis.asyncio as redis
 import time
 import os
+ 
+class Subscriber:
+	def __init__(self,*args, **kwargs):
+		self.pubs=kwargs.get('KNOWN_PUBS')
 
-KNOWN_NODES = ["node-1", "node-2", "node-3"]
+	async def subAgent(self,node: str):
+		# split argument input to match publisher's node's:
+		# node-1_ActivePower --> ['node-1', 'ActivePower']
+		split_str = node.split('_')  # ('NODE_NAME','VALUE_NAME')
 
+		# Connection to redis machine
+		r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
-async def subAgent(node: str):
-    # split argument input to match publisher's node's:
-    # node-1_ActivePower --> ['node-1', 'ActivePower']
-    split_str = node.split('_')  # ('NODE_NAME','VALUE_NAME')
+		# get other node names
+		hosts = self.pubs.copy()
+		if split_str[0] in hosts:
+			index = hosts.index(split_str[0])
+			hosts.pop(index)
 
-    # Connection to redis machine
-    r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
-
-    # get other node names
-    hosts = KNOWN_NODES.copy()
-    if split_str[0] in hosts:
-        index = hosts.index(split_str[0])
-        hosts.pop(index)
-
-    # subscribing to specified node
-    async with r.pubsub() as ps:
-        # subscribe to own channel
-        await ps.subscribe(split_str[0])
-        # print(f"[{time.strftime('%X')}]: Subscribed to {split_str[0]}")
-        while True:
-            message = await ps.get_message(ignore_subscribe_messages=True, timeout=7)
-            # if message NOT empty
-            if message is not None:
-                # If incoming message, break loop and finish agent
-                if message['data'] == "STOP":
-                    print(f"[{time.strftime('%X')}]: EOF")
-                    break
-                stream = (message['channel'], message['data'])
-                # print(stream)
-                time.sleep(0.001)  # be nice to the system :)
-                return stream, hosts
-            else:
-                continue
-
-
-def parse(data):
-    '''Parse incoming data'''
-    value = data[1]  # date value
-    value = value.split('\n')
-    return value
+		# subscribing to specified node
+		async with r.pubsub() as ps:
+			# subscribe to own channel
+			await ps.subscribe(split_str[0])
+			# print(f"[{time.strftime('%X')}]: Subscribed to {split_str[0]}")
+			while True:
+				message = await ps.get_message(ignore_subscribe_messages=True, timeout=7)
+				# if message NOT empty
+				if message is not None:
+					# If incoming message, break loop and finish agent
+					if message['data'] == "STOP":
+						print(f"[{time.strftime('%X')}]: EOF")
+						break
+					stream = (message['channel'], message['data'])
+					# print(stream)
+					time.sleep(0.001)  # be nice to the system :)
+					return stream, hosts
+				else:
+					continue
 
 
-async def main():
-    '''Compose subscribers for async data gathering'''
-    print(f"started at {time.strftime('%X')}")
-    while True:
-        results = await asyncio.gather(subAgent("node-1_ActivePower"),
-                                       subAgent("node-2_WindSpeed"),
-                                       subAgent("node-3_WindDir"))
-        if results[0][0][1] == "STOP" and results[1][0][1] == "STOP" and results[2][0][1] == "STOP":
-            break
-        node_1_value = parse(results[0][0])
-        node_2_value = parse(results[1][0])
-        node_3_value = parse(results[2][0])
+	def parse(self,data):
+		'''Parse incoming data'''
+		value = data[1]  # date value
+		value = value.split('\n')
+		return value
 
-        # Check if all nodes have same date
-        if node_1_value[0] == node_2_value[0] == node_3_value[0]:
-            print(f"[{time.strftime('%X')}] Dates are same:\nnode-1: {node_1_value[1]}\tnode-2: {node_2_value[1]}\tnode-3: {node_3_value[1]}\n")
-            # Write to file
-            filename = "combined.txt"
 
-            # Check if file exist because we want to write to first row
-            # as column names (headers)
-            flag = False
-            if os.path.exists(filename) == False:
-                flag = True
+	async def main(self):
+		'''Compose subscribers for async data gathering'''
+		print(f"started at {time.strftime('%X')}")
+		while True:
+			results = await asyncio.gather(self.subAgent("node-1_ActivePower"),
+											self.subAgent("node-2_WindSpeed"),
+											self.subAgent("node-3_WindDir"))
+			if results[0][0][1] == "STOP" and results[1][0][1] == "STOP" and results[2][0][1] == "STOP":
+				break
+			node_1_value = self.parse(results[0][0])
+			node_2_value = self.parse(results[1][0])
+			node_3_value = self.parse(results[2][0])
 
-                # I am opening and closing each time value incomes. I want to read
-                # file in realtime(even though it is not performant), otherwise
-                # can't read file because of the lock
-            async with aiofiles.open(filename, 'a+', encoding='utf-8') as f:
-                if flag:
-                    await f.write(f"Execution time\tDate/Time\tActivePower\tWindSpeed\tWindDir\n")
-                await f.write(f"{time.strftime('%X')}\t{node_1_value[0]}\t{node_1_value[1]}\t{node_2_value[1]}\t{node_3_value[1]}\n")
-    print(f"finished at {time.strftime('%X')}")
+			# Check if all nodes have same date
+			if node_1_value[0] == node_2_value[0] == node_3_value[0]:
+				print(f"[{time.strftime('%X')}] Dates are same:\nnode-1: {node_1_value[1]}\tnode-2: {node_2_value[1]}\tnode-3: {node_3_value[1]}\n")
+				# Write to file
+				filename = "combined.txt"
+
+				# Check if file exist because we want to write to first row
+				# as column names (headers)
+				flag = False
+				if os.path.exists(filename) == False:
+					flag = True
+				
+				'''
+					I am opening and closing each time value incomes. I want to read
+					file in realtime(even though it is not performant), otherwise
+					can't read file because of the lock
+				'''
+				async with aiofiles.open(filename, 'a+', encoding='utf-8') as f:
+					if flag:
+						await f.write(f"Execution time\tDate/Time\tActivePower\tWindSpeed\tWindDir\n")
+					await f.write(f"{time.strftime('%X')}\t{node_1_value[0]}\t{node_1_value[1]}\t{node_2_value[1]}\t{node_3_value[1]}\n")
+		print(f"finished at {time.strftime('%X')}")
 
 if __name__ == "__main__":
-    asyncio.run(main())
+	KNOWN_PUBS = ["node-1", "node-2", "node-3"]
+	sub=Subscriber(KNOWN_PUBS=KNOWN_PUBS)
+	asyncio.run(sub.main())
